67e3fcb0b6c8d905c6bf421f4291e92f4a10842c
WSW <2544894086@qq.com>
2019-09-14 17:23:50 +0800

    把数字翻译成字符串

--- src/main/java/com/wsw/leetcode/niuke/N32.java
+++ src/main/java/com/wsw/leetcode/niuke/N32.java
@@ -1,7 +1,15 @@
 package com.wsw.leetcode.niuke;
 
+import java.util.Scanner;
+
 /**
  * Created by wsw on 2019/9/14 17:19
+ * 把数字翻译成字符串
  */
 public class N32 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+    }
 }
